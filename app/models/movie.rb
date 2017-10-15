63be105484796a62eb5ac3de2c2238a898a0d591
Vincent Lopez <vincent-lopez@uiowa.edu>
2017-10-15 18:27:29 +0000

    api key

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,9 @@ class Movie < ActiveRecord::Base
   
  class Movie::InvalidKeyError < StandardError ; end
   
+  def self.api_key
+    'f4702b08c0ac6ea5b51425788bb26562'
+  end
   def self.find_in_tmdb(string)
     begin
       Tmdb::Movie.find(string)
